8676fb7a00cce835c08948bdd4e34f93ca253549
Derrick <dryan6@binghamton.edu>
2021-03-01 19:51:32 -0500

    Added drawing features

---
+++ draw/draw/Draw.h
@@ -0,0 +1,17 @@
+//
+//  Draw.h
+//  draw
+//
+//  Created by Derrick Ryan on 3/1/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Draw : UIView
+@property (nonatomic) float x, y;
+@property (nonatomic) int count;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ draw/draw/Draw.m
@@ -0,0 +1,42 @@
+//
+//  Draw.m
+//  draw
+//
+//  Created by Derrick Ryan on 3/1/21.
+//
+
+#import "Draw.h"
+
+@implementation Draw
+@synthesize x, y;
+@synthesize count;
+
+// custom drawing
+- (void)drawRect:(CGRect)rect {
+    CGRect bounds = [self bounds];
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    //background color
+    if(count > 3)
+        [[UIColor blackColor] setFill];
+    else
+        [[UIColor blueColor] setFill];
+    CGContextFillRect(context, bounds);
+    
+    //circle color
+    [[UIColor redColor] setFill];
+    CGContextFillEllipseInRect(context, CGRectMake(x, y, 30, 30));
+}
+
+// touches on the screen
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    for(UITouch *t in touches){
+        CGPoint p = [t locationInView:self];
+        x = p.x;
+        y = p.y;
+        [self setNeedsDisplay];
+        count++;
+    }
+}
+
+@end
